c307f2c819f641abdd41c1e6bfac2aea4676abe0
rxt30 <mhampel@pm.me>
2024-02-23 00:40:21 +0100

    Remove unstable for now

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,27 +1,14 @@
 { config, pkgs, ... }:
-let
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-in
 {
-  nixpkgs.config = {
-    packageOverrides = pkgs: {
-      unstable = import unstableTarball {
-        config = config.nixpkgs.config;
-      };
-    };
-  };
   environment.systemPackages = with pkgs; [
-    unstable.neovim
-    unstable.vim
+    neovim
+    vim
     curl
-    unstable.eza
     git
-    unstable.python
+    python
     sudo
     nmtui
-    unstable.nodejs
+    nodejs
     xterm
   ];
 }
