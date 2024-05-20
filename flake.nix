9556ee454303277349a9248c95a00851081075af
rxt30 <mhampel@pm.me>
2024-05-20 11:30:11 +0200

    Fixed vm config

--- flake.nix
+++ flake.nix
@@ -20,56 +20,63 @@
     # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    hyprlock,
-    hypridle,
-    hyprpaper,
-    musnix,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      maxh-nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [
-        musnix.nixosModules.musnix
-        ./nixos/maxh/configuration.nix
-        ];
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , hyprlock
+    , hypridle
+    , hyprpaper
+    , musnix
+    , ...
+    } @ inputs:
+    let
+      inherit (self) outputs;
+    in
+    {
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        maxh-nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          # > Our main nixos configuration file <
+          modules = [
+            musnix.nixosModules.musnix
+            ./nixos/maxh/configuration.nix
+          ];
+        };
+        mime-nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          # > Our main nixos configuration file <
+          modules = [ ./nixos/mime/configuration.nix ];
+        };
       };
-      mime-nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [./nixos/mime/configuration.nix];
-      };
-    };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "maxh@maxh-nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [
-        hyprlock.homeManagerModules.hyprlock
-        hypridle.homeManagerModules.hypridle
-        hyprpaper.homeManagerModules.hyprpaper
-        ./home-manager/maxh.nix
-        ];
-      };
-      "mime@maxh-nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/mime.nix];
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        "maxh@maxh-nixos" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          # > Our main home-manager configuration file <
+          modules = [
+            hyprlock.homeManagerModules.hyprlock
+            hypridle.homeManagerModules.hypridle
+            hyprpaper.homeManagerModules.hyprpaper
+            ./home-manager/maxh.nix
+          ];
+        };
+        "mime@maxh-nixos" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          # > Our main home-manager configuration file <
+          modules = [
+            hyprlock.homeManagerModules.hyprlock
+            hypridle.homeManagerModules.hypridle
+            hyprpaper.homeManagerModules.hyprpaper
+            ./home-manager/mime.nix
+          ];
+        };
       };
     };
-  };
 }
